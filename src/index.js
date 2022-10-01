3296640063f44c0fe991b4900f895e013020aaaa
Ben West <benjwest8@gmail.com>
2022-10-01 09:15:46 -0600

    lifting state up

--- src/index.js
+++ src/index.js
@@ -2,29 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
+class Square extends React.Component {    
+    render() {
+        return (
+            <button 
+                className="square" 
+                onClick={() => this.props.onClick()}
+            >
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+  
+  class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          value: null,
+            squares: Array(9).fill(null),
         };
     }
 
-    render() {
-      return (
-        <button 
-        className="square" 
-        onClick={() => this.setState({value: 'X'})}
-        >
-          {this.state.value}
-        </button>
-      );
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
     }
-  }
-  
-  class Board extends React.Component {
+    
     renderSquare(i) {
-      return <Square  value={i}/>;
+        return (
+            <Square 
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
   
     render() {
